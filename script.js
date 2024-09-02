8ffdf265e4836f4865bcb22c4c1a0fa395fbff8b
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-02 15:48:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function Car(make, model) {
 }
 
 function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model){
+	Car.call(this,make,model)
 		this.topspeed = topSpeed;
-	}
-
+	
+}
 	SportsCar.prototype = Object.create(Car.prototype);
 	SportsCar.prototype.constructor = SportsCar;
 
 	SportsCar.prototype.getTopspeed=function () {
 		this.topspeed = topSpeed;
 	}
-}
+
 
 // Do not change the code below
 window.Car = Car;
