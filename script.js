fabe17c7eb9226e7e11834e3d1d84544e5814ea3
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-02 16:32:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function SportsCar(make, model, topSpeed) {
 	SportsCar.prototype = Object.create(Car.prototype);
 	SportsCar.prototype.constructor = SportsCar;
 
-SportsCar.prototype.getTopSpeed = function(){ // corrected here
+SportsCar.prototype.getTopSpeed = function(){ 
 	return this.topspeed;
 }		
 
